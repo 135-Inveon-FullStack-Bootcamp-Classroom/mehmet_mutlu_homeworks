2689dc8d2a7e20ba9b789da307e2fdb58ad5482b
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-20 01:02:25 +0300

    Fifth project was completed.

---
+++ 5-CSharpCarTypes/4-CSharpCarTypes/Cars.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5_CSharpCarTypes
+{
+    public interface ICar
+    {
+        public string Color { get; set; }
+        public int ModelYear { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Mercedes : ICar
+    {
+        public string Brand = "Mercedes";
+        public string Color { get; set; }
+        public int ModelYear { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class Audi : ICar
+    {
+        public string Brand = "Mercedes";
+        public string Color { get; set; }
+        public int ModelYear { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class BMW : ICar
+    {
+        public string Brand = "Mercedes";
+        public string Color { get; set; }
+        public int ModelYear { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class MercedesA180 : Mercedes
+    {
+        public string Model = "A180";
+        public string CarType = "Hatchback";
+    }
+
+    public class AudiA7 : Audi
+    {
+        public string Model = "A7";
+        public string CarType = "Coupe";
+    }
+
+    public class BMW520d : BMW
+    {
+        public string Model = "520d";
+        public string CarType = "Sedan";
+    }
+}
---
+++ 5-CSharpCarTypes/4-CSharpCarTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5_CSharpCarTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MercedesA180 mercedesA180 = new MercedesA180() { Color = "White", ModelYear = 2020, Price = 750000 };
+            AudiA7 audiA7 = new AudiA7() { Color = "Black", ModelYear = 2020, Price = 1000000 };
+            BMW520d bmw520d = new BMW520d() { Color = "Blue", ModelYear = 2021, Price = 1200000 };
+
+            Console.WriteLine(mercedesA180.Model + " " + mercedesA180.Brand + " " + mercedesA180.ModelYear);
+            Console.WriteLine(mercedesA180.CarType);
+            Console.WriteLine(mercedesA180.Color);
+            Console.WriteLine(mercedesA180.Price);
+
+            Console.WriteLine("-----");
+
+            Console.WriteLine(audiA7.Model + " " + audiA7.Brand + " " + audiA7.ModelYear);
+            Console.WriteLine(audiA7.CarType);
+            Console.WriteLine(audiA7.Color);
+            Console.WriteLine(audiA7.Price);
+
+            Console.WriteLine("-----");
+
+            Console.WriteLine(bmw520d.Model + " " + bmw520d.Brand + " " + bmw520d.ModelYear);
+            Console.WriteLine(bmw520d.CarType);
+            Console.WriteLine(bmw520d.Color);
+            Console.WriteLine(bmw520d.Price);
+
+            Console.ReadLine();
+        }
+    }
+}
