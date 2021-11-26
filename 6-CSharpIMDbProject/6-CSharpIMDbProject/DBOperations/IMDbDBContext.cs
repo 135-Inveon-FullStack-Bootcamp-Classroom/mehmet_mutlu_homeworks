a5c3830e42d940a83f617c954d98d802762a2047
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-26 23:42:15 +0300

    EntityFrameworkCore was installed and IMDbDBContext was created

---
+++ 6-CSharpIMDbProject/6-CSharpIMDbProject/DBOperations/IMDbDBContext.cs
@@ -0,0 +1,23 @@
+﻿using _6_CSharpIMDbProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6_CSharpIMDbProject.DBOperations
+{
+    public class IMDbDBContext:DbContext
+    {
+        public IMDbDBContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Actor> Actor { get; set; }
+        public DbSet<Director> Director { get; set; }
+
+    }
+}
