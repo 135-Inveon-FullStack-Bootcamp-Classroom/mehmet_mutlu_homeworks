1072770ba75acd50ff21900564ced3d1316ace61
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-27 10:07:23 +0300

    Http requests were created for Directors

---
+++ 6-CSharpIMDbProject/6-CSharpIMDbProject/Controllers/DirectorsController.cs
@@ -0,0 +1,58 @@
+﻿using _6_CSharpIMDbProject.Data.Services;
+using _6_CSharpIMDbProject.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6_CSharpIMDbProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectorsController : ControllerBase
+    {
+        public DirectorService _directorService;
+
+        public DirectorsController(DirectorService directorService)
+        {
+            _directorService = directorService;
+        }
+
+        [HttpGet("get-all-directors")]
+        public IActionResult GetAllDirectors()
+        {
+            var allDirectors = _directorService.GetAllDirectors();
+            return Ok(allDirectors);
+        }
+
+        [HttpGet("get-director-by-id/{id}")]
+        public IActionResult GetDirectorByID(int id)
+        {
+            var Director = _directorService.GetDirectorByID(id);
+            return Ok(Director);
+        }
+
+        [HttpPost("add-director")]
+        public IActionResult AddDirector([FromBody] DirectorVM director)
+        {
+            _directorService.AddDirector(director);
+            return Ok();
+        }
+
+        [HttpPut("update-director-by-id/{id}")]
+        public IActionResult UpdateDirectorByID(int id, [FromBody] DirectorVM director)
+        {
+            var updatedDirector = _directorService.UpdatedirectorByID(id, director);
+            return Ok(updatedDirector);
+        }
+
+        [HttpDelete("delete-director-by-id/{id}")]
+        public IActionResult DeleteDirectorByID(int id)
+        {
+            _directorService.DeletedirectorByID(id);
+            return Ok();
+        }
+    }
+}
---
+++ 6-CSharpIMDbProject/6-CSharpIMDbProject/Data/Services/DirectorService.cs
@@ -0,0 +1,69 @@
+﻿using _6_CSharpIMDbProject.Data.ViewModels;
+using _6_CSharpIMDbProject.DBOperations;
+using _6_CSharpIMDbProject.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6_CSharpIMDbProject.Data.Services
+{
+    public class DirectorService
+    {
+        private IMDbDBContext _context;
+
+        public DirectorService(IMDbDBContext context)
+        {
+            _context = context;
+        }
+
+        public void AddDirector(DirectorVM director)
+        {
+            var _director = new Director()
+            {
+                FirstName = director.FirstName,
+                LastName = director.LastName,
+                MovieID = director.MovieID
+            };
+
+            _context.Directors.Add(_director);
+            _context.SaveChanges();
+        }
+
+        public List<Director> GetAllDirectors()
+        {
+            return _context.Directors.ToList();
+        }
+
+        public Director GetDirectorByID(int directorID)
+        {
+            return _context.Directors.FirstOrDefault(director => director.ID == directorID);
+        }
+
+        public Director UpdatedirectorByID(int directorID, DirectorVM director)
+        {
+            var _director = _context.Directors.FirstOrDefault(director => director.ID == directorID);
+            if (_director != null)
+            {
+                _director.FirstName = director.FirstName;
+                _director.LastName = director.LastName;
+                _director.MovieID = director.MovieID;
+
+                _context.SaveChanges();
+            }
+
+            return _director;
+        }
+
+        public void DeletedirectorByID(int directorID)
+        {
+            var _director = _context.Directors.FirstOrDefault(director => director.ID == directorID);
+            if (_director != null)
+            {
+                _context.Directors.Remove(_director);
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
